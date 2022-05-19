cbee51b72f11739841c9368d0ef0fbd6ddbaba61
broccoliass <biscuitchocolate17@gmail.com>
2022-05-19 07:28:35 +0000

    read and pass value addresses

--- wakeonlan.c
+++ wakeonlan.c
@@ -8,32 +8,49 @@
 #include <arpa/inet.h>
 
 int main(int argc, const char* argv[]){
-    int i = 0;
-	int broadcast = 1;
-    int socket_desc;
+	int i = 0;
+    	int broadcast = 1;
+    	int socket_desc;
 
-    char broadcastAddress[16] = "";
+    	char broadcastAddress[16] = "";
 
-    struct sockaddr_in clientSide;
-    struct sockaddr_in serverSide;
+    	struct sockaddr_in clientSide;
+    	struct sockaddr_in serverSide;
+
+    	unsigned char packet[102];
+    	unsigned int mac[6];
 
 	if(argc < 2){
 		printf("No input\n");
 		exit(EXIT_FAILURE);
 	}
 
-    socket_desc = socket(AF_INET, SOCK_DGRAM, 0);
-  
+    	i = sscanf(argv[1],"%x:%x:%x:%x:%x:%x", &(mac[0]), &(mac[1]), &(mac[2]), &(mac[3]), &(mac[4]), &(mac[5]));
+	if(i != 6){
+		printf("Incorrect mac address.\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("\nPacket will sent to %02hhx:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
-    clientSide.sin_family = AF_INET;
-    clientSide.sin_addr.s_addr = INADDR_ANY;
-    clientSide.sin_port = 0;
+    	if(argc > 2){
+		i = sscanf(argv[2],"%d.%d.%d.%d", &i, &i, &i, &i);
+		if(i == 4){
+			strcpy(broadcastAddress, argv[2]);
+		}
+	}
 
-    bind(socket_desc, (struct sockaddr*) &clientSide, sizeof(clientSide));
+    	socket_desc = socket(AF_INET, SOCK_DGRAM, 0);
+  	//socket setup not yet
 
-    serverSide.sin_family = AF_INET;
-    serverSide.sin_addr.s_addr = inet_addr(broadcastAddress);
-    serverSide.sin_port = htons(9);
+    	clientSide.sin_family = AF_INET;
+    	clientSide.sin_addr.s_addr = INADDR_ANY;
+    	clientSide.sin_port = 0;
+
+    	bind(socket_desc, (struct sockaddr*) &clientSide, sizeof(clientSide));
+
+    	serverSide.sin_family = AF_INET;
+    	serverSide.sin_addr.s_addr = inet_addr(broadcastAddress);
+    	serverSide.sin_port = htons(9);
 
 	printf("\nWaking up (mac address)");
 	exit(EXIT_SUCCESS);
