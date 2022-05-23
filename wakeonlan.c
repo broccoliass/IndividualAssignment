f63f6362a94bc8988d54b02eedefd7e91b476e6c
broccoliass <biscuitchocolate17@gmail.com>
2022-05-23 02:54:20 +0000

    pass mac address

--- wakeonlan.c
+++ wakeonlan.c
@@ -75,6 +75,6 @@ int main(int argc, const char* argv[]){
 
         sendto(socket_desc, &packet, sizeof(unsigned char) * 102, 0, (struct sockaddr*) &serverSide, sizeof(serverSide));
 
-	printf("\nSend packet to (mac address)");
+	printf("\nSend packet to (mac address) %02hhx:%02x:%02x:%02x:%02x:%02x\n\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 	exit(EXIT_SUCCESS);
 }
